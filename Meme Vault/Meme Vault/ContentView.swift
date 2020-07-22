2a4c88e0bb5b899b2e55587eaec3f02d40bfe8e4
Isvvc <lyons@tuta.io>
2020-07-22 17:32:30 -0600

    Create empty project

---
+++ Meme Vault/Meme Vault/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Meme Vault
+//
+//  Created by Isaac Lyons on 7/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meme Vault/Meme Vault/Meme_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_VaultApp.swift
+//  Meme Vault
+//
+//  Created by Isaac Lyons on 7/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Meme_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

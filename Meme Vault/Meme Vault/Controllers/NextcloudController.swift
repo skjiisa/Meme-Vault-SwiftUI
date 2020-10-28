23a114b776bfd496021c7b7f60b522a8d25e73e5
Isvvc <lyons@tuta.io>
2020-10-28 17:46:57 -0600

    Allow Account objects to be created and deleted

--- Meme Vault/Meme Vault/Controllers/NextcloudController.swift
+++ Meme Vault/Meme Vault/Controllers/NextcloudController.swift
@@ -11,6 +11,22 @@ import KeychainSwift
 
 class NextcloudController: ObservableObject {
     
+    //MARK: Account CRUD
     
+    func createAccount(context: NSManagedObjectContext) -> Account {
+        let newAccount = Account(context: context)
+        try? context.save()
+        return newAccount
+    }
+    
+    func delete(account: Account, context: NSManagedObjectContext) {
+        context.delete(account)
+        try? context.save()
+    }
+    
+    func delete(accounts: [Account], context: NSManagedObjectContext) {
+        accounts.forEach { context.delete($0) }
+        try? context.save()
+    }
     
 }
--- Meme Vault/Meme Vault/Views/AccountsView.swift
+++ Meme Vault/Meme Vault/Views/AccountsView.swift
@@ -8,16 +8,56 @@
 import SwiftUI
 
 struct AccountsView: View {
-    @FetchRequest(sortDescriptors: [NSSortDescriptor(key: "username", ascending: true)])
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(key: "username", ascending: true)], animation: .spring())
     var accounts: FetchedResults<Account>
     
+    @EnvironmentObject var nextcloudController: NextcloudController
+    
+    @State private var selectedAccount: Account?
+    @State private var newAccount: Account?
+    
     var body: some View {
-        List {
-            ForEach(accounts) { account in
-                Text(account.username ?? "Account")
+        Form {
+            Section {
+                ForEach(accounts) { account in
+                    NavigationLink(destination: LoginView(account: account), tag: account, selection: $selectedAccount) {
+                        VStack(alignment: .leading) {
+                            Text(account.username ?? "")
+                            if let baseURL = account.baseURL,
+                               !baseURL.isEmpty {
+                                Text(baseURL)
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+                .onDelete { indexSet in
+                    nextcloudController.delete(accounts: indexSet.map { accounts[$0] }, context: moc)
+                }
+            }
+            
+            Section {
+                Button() {
+                    self.newAccount = nextcloudController.createAccount(context: moc)
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("New Account")
+                        Spacer()
+                    }
+                }
             }
         }
         .navigationTitle("Accounts")
+        .sheet(item: $newAccount) { newAccount in
+            NavigationView {
+                LoginView(account: newAccount)
+                    .environment(\.managedObjectContext, moc)
+                    .environmentObject(nextcloudController)
+            }
+        }
     }
 }
 
